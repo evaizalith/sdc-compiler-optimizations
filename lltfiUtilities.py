01070b1c97f7814f83e935a78e4fdec405e60d56
FoundIzalith <evapowlison@protonmail.com>
2024-02-27 14:45:14 -0500

    Add compileAndRun()

---
+++ lltfiUtilities.py
@@ -0,0 +1,29 @@
+import os
+
+def compileAndRun(filename, llfiRoot):
+    os.system('rm -rf ./llfi*')
+
+    comp = "clang++ -w -emit-llvm -fno-unroll-loops -lstdc++ -fno-use-cxa-atexit -S *.cc"
+    os.system(comp)
+
+    link = "llvm-link -o \"" + filename + ".ll\" -S *.ll"
+    os.system(link)
+
+    opt = "\"" + filename + ".ll\" --disable-preinline -time-passes -S -o \"" + filename + ".ll\""
+    os.system(opt)
+
+    instrument = llfiRoot + "/bin/instrument -lstdc++ --readable \"" + filename + ".ll\""
+    os.system(instrument)
+
+    profile = llfiRoot + "/bin/profile ./llfi/\"" + filename + "-profiling.exe\" 10"
+    os.system(profile)
+
+    injection = llfiRoot + "/bin/injectfault ./llfi/\"" + filename + "-faultinjection.exe\" 10"
+    os.system(injection)
+
+def interpretFIResults():
+    temp = "coming soon" 
+
+if __name__ == "__main__":
+    compileAndRun()
+    interpretFIResults()
