a9b7fc9d8ae1307883c57a1081e2be1688c63a2c
FoundIzalith <evapowlison@protonmail.com>
2024-03-27 12:45:37 -0400

    Add training framework

--- main.py
+++ main.py
@@ -37,7 +37,29 @@ def main():
 
     print("Device is", DEVICE)
 
-    opts = getOptimizations(optsFile)
+    opts = torch.tensor(getOptimizations(optsFile))
+
+    model = sdcModel()
+
+    if exists ('trained_model.pt'):
+        model.load_state_dict(torch.load('trained_model.pt'))
+        print("Loading existing model")
+    else:
+        print("No existing model found; training from scratch")
+
+    model = model.to(DEVICE)
+
+    for epoch in range(10):
+        print("Epoch ", epoch)
+
+        errorRate = sdcModel.train(model, dl_train, optimizer, criterion, DEVICE)
+        print("Error rate = ", errorRate)
+
+        print()
+
+    torch.save(model.state_dict(), "trained_model.pt")
+
+    print("All done!")
 
 if __name__ == "__main__":
     main()
