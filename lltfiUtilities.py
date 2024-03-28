29603d79fc45bac1e72facd01d148f77fa607d08
FoundIzalith <evapowlison@protonmail.com>
2024-03-28 17:04:53 -0400

    Switch to f-strings for readability

--- lltfiUtilities.py
+++ lltfiUtilities.py
@@ -6,24 +6,24 @@ def compileAndRun(filename, llfiRoot, llvmRoot):
     comp = "clang++ -w -emit-llvm -fno-unroll-loops -lstdc++ -fno-use-cxa-atexit -S *.cc"
     os.system(comp)
 
-    link = llvmRoot + "llvm-link -o \"" + filename + ".ll\" -S *.ll"
+    link = f"{llvmRoot}llvm-link -o {filename}.ll -S *.ll"
     os.system(link)
 
-    opt = llvmRoot + "opt " + "\"" + filename + ".ll\" --disable-preinline -time-passes -S -o \"" + filename + ".ll\""
+    opt = f"{llvmRoot}opt \"{filename}.ll\" --disable-preinline -time-passes -S -o {filename}.ll"
     os.system(opt)
 
-    instrument = llfiRoot + "/bin/instrument -lstdc++ --readable \"" + filename + ".ll\""
+    instrument = f"{llfiRoot}/bin/instrument -lstdc++ --readable \"{filename}.ll\""
     os.system(instrument)
 
-    profile = llfiRoot + "/bin/profile ./llfi/\"" + filename + "-profiling.exe\" 10"
+    profile = f"{llfiRoot}/bin/profile ./llfi/\"{filename}-profile.exe\" 10"
     os.system(profile)
 
-    injection = llfiRoot + "/bin/injectfault ./llfi/\"" + filename + "-faultinjection.exe\" 10"
+    injection = f"{llfiRoot}/bin/injectfault ./llfi/\"{filename}-faultinjection.exe\" 10"
     os.system(injection)
 
 def interpretFIResults():
     temp = "coming soon" 
 
 if __name__ == "__main__":
-    compileAndRun()
+    compileAndRun("test", ".", "~/llvm-project/build/bin/") 
     interpretFIResults()
